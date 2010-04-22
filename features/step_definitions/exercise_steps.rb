129065b71302fdc259f4072fd66620328d40a290
Francisco Licea <francisco.licea@gmail.com>
2010-04-22 11:27:42 -0600

    move exercise steps from gradable to exercise_steps.steps

---
+++ features/step_definitions/exercise_steps.rb
@@ -0,0 +1,13 @@
+Given /^I have finished "([^\"]*)" with a "([^\"]*)"$/ do |title, grade|
+  exercise = Exercise.find_by_title title
+  exercise.grade_sheets.create! :grade=>grade.to_f, :user=>@current_user, :exercise=>exercise
+end
+
+Given /^"([^\"]*)" has the grades "([^\"]*)"$/ do |exercise_title, grades|
+  exercise = Exercise.find_by_title exercise_title
+  grades = grades.split.collect {|g| g.to_f}
+  0.upto(grades.count - 1) do |n|
+    user = User.create! :username=>"user#{n}", :password=>"password", :password_confirmation=>'password', :email=>"user#{n}@mail.com"
+    exercise.grade_sheets.create! :grade=>grades[n], :user=>user, :exercise=>exercise
+  end
+end
