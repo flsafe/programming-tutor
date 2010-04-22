37f6aedd1ec0bcfc8010af339d91edc5103da7ec
Francisco Licea <francisco.licea@gmail.com>
2010-04-22 13:14:19 -0600

    exercise_set steps now use factories

--- features/step_definitions/exercise_set_steps.rb
+++ features/step_definitions/exercise_set_steps.rb
@@ -1,14 +1,13 @@
 Given /^there exists an exercise set "([^\"]*)" with "([^\"]*)" and "([^\"]*)"$/ do |set_title, ex1_title, ex2_title|
-  @ex1 = Exercise.create! :title=>ex1_title, :description=>"#{@ex1} description"
-  @ex2 = Exercise.create :title=>ex2_title, :description=>"#{@ex2} description"
-  @exercise_set = ExerciseSet.create! :title=>set_title, :description=>"#{set_title} description", :exercises=>[@ex1, @ex2]
+  @ex1 = Factory.create :exercise, :title=>ex1_title
+  @ex2 = Factory.create :exercise, :title=>ex2_title
+  @exercise_set = Factory.create :exercise_set, :title=>set_title, :exercises=>[@ex1, @ex2]
 end
 
 Given /^"([^\"]*)" users have done "([^\"]*)"$/ do |n_users, title|
   exercise_set = ExerciseSet.find_by_title title
   1.upto(n_users.to_i) do |n|
-    user = User.create! :username=>"user#{n}", :password=>"password", :password_confirmation=>'password', :email=>"user#{n}@mail.com"
-    SetGradeSheet.create! :grade=>100, :user=>user, :exercise_set=>exercise_set
+    Factory.create :set_grade_sheet, :exercise_set=>exercise_set
   end
 end
 
@@ -20,14 +19,7 @@ end
 
 Given /^I have finished "([^\"]*)" with an average of "([^\"]*)"$/ do |title, avg_grade|
   exercise_set = ExerciseSet.find_by_title title
-  
-  grade_sheet = SetGradeSheet.new
-  grade_sheet.user = @current_user
-  grade_sheet.exercise_set = exercise_set
-  grade_sheet.grade = avg_grade
-  grade_sheet.save
-  
-  exercise_set.set_grade_sheets << grade_sheet
+  g = Factory.create :set_grade_sheet, :exercise_set=>exercise_set, :grade=>avg_grade
 end
 
 When /^I view exercise set "([^\"]*)"$/ do |title|
