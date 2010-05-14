23891176607311e99a732c4bc79ce99ff5c06029
Francisco Licea <francisco.licea@gmail.com>
2010-05-14 14:45:48 -0600

    Extend User#grade_for? to accept exercise sets

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,20 @@ class User < ActiveRecord::Base
   attr_protected :roles_mask
   
   def grade_for?(exercise)
-    if gs = grade_sheets.find(:first, :conditions=>['exercise_id=?', exercise.id], :order=>'created_at DESC') then
-      gs.grade
+    if is_exercise? exercise
+      grade_sheets_or_set_grade_sheets =  :grade_sheets
+      conds                            = ['exercise_id=?', exercise.id]
+    else
+      grade_sheets_or_set_grade_sheets = :set_grade_sheets
+      conds = ['exercise_set_id=?', exercise.id]
     end
+    gs = send(grade_sheets_or_set_grade_sheets).find(:first, :conditions=>conds, :order=>'created_at DESC')
+    gs.grade if gs
+  end
+  
+  private
+  
+  def is_exercise?(ex)
+    ex.respond_to?(:exercise_set)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,39 @@
 require 'spec_helper'
 
 describe User do
+  
   describe  "#grade_for" do
+    
     before(:each) do
-      @current_user = Factory.create :user
-      @exercise = Factory.create(:complete_exercise_set).exercises[0]
+        @current_user = Factory.create :user
+        @exercise_set = Factory.create :complete_exercise_set
+        @exercise = @exercise_set.exercises[0]
     end
     
-    it "returns the grade for the associated exercise" do
-      @exercise.grade_sheets.create! :grade=>92.1, :user=>@current_user, :exercise=>@exercise
-      @current_user.grade_for?(@exercise).should == 92.1
+    context "when given an exercise" do
+      it "returns the grade for the associated exercise" do
+        @exercise.grade_sheets.create! :grade=>92.1, :user=>@current_user, :exercise=>@exercise
+        @current_user.grade_for?(@exercise).should == 92.1
+      end
+    
+      it "returns the most recent grade" do
+       GradeSheet.create! :grade=>60, :user=>@current_user, :exercise=>@exercise
+       GradeSheet.create! :grade=>90, :user=>@current_user, :exercise=>@exercise
+       sleep(1) #Make sure one is inserted at a later time
+       GradeSheet.create! :grade=>100, :user=>@current_user, :exercise=>@exercise
+       @current_user.grade_for?(@exercise).should == 100
+      end
+    
+      it "returns nil if there is no grade sheet associated with the exercise" do
+        @current_user.grade_for?(@exercise).should == nil
+      end
     end
     
-    it "returns the most recent grade" do
-     GradeSheet.create! :grade=>60, :user=>@current_user, :exercise=>@exercise
-     GradeSheet.create! :grade=>90, :user=>@current_user, :exercise=>@exercise
-     sleep(1) #Make sure one is inserted at a later time
-     GradeSheet.create! :grade=>100, :user=>@current_user, :exercise=>@exercise
-     @current_user.grade_for?(@exercise).should == 100
-    end
-    
-    it "returns nil if there is no grade sheet associated with the exercise" do
-      @current_user.grade_for?(@exercise).should == nil
+    context "when given an exercise set" do
+      it "returns the average grade for the associated exercise set" do
+        @exercise_set.set_grade_sheets.create! :grade=>91.1, :user=>@current_user, :exercise_set=>@exercise_set
+        @current_user.grade_for?(@exercise_set).should == 91.1
+      end
     end
   end
 end
