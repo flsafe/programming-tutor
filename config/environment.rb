39492703530355f178dcdfe02961884f77c20dc0
Francisco Licea <francisco.licea@gmail.com>
2010-05-18 12:50:03 -0600

    Install delayed_job

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   config.gem 'acts-as-taggable-on'
   config.gem 'easy_roles'
   config.gem 'paperclip'
+  config.gem 'delayed_job'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
