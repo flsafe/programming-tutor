fc2f91cc14b84aabf26c1cf2068f45540992a6a9
Francisco Licea <francisco.licea@gmail.com>
2010-05-20 16:08:14 -0600

    Remove :code from the SyntaxCheckResult object

--- lib/syntax_check_job.rb
+++ lib/syntax_check_job.rb
@@ -3,7 +3,7 @@ class SyntaxCheckJob < Struct.new :code, :user_id, :exercise_id
   attr_accessor :code, :user_id, :exercise_id, :error_message
   
   def perform
-    @error_message = `echo "#{code}" | gcc -x c -fsyntax-only - 2>&1`
-    SyntaxCheckResult.create! self.attributes
+    self.error_message = `echo "#{code}" | gcc -x c -fsyntax-only - 2>&1`
+    SyntaxCheckResult.create! :user_id=>user_id, :exercise_id=>exercise_id, :error_message=>error_message
   end
 end
