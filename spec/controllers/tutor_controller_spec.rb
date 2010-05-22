42b0322caf45e5b980b64725c19be5f44f8b0a97
Francisco Licea <francisco.licea@gmail.com>
2010-05-22 14:53:10 -0600

    Rename specs to be more descriptive

--- spec/controllers/tutor_controller_spec.rb
+++ spec/controllers/tutor_controller_spec.rb
@@ -4,17 +4,15 @@ describe TutorController do
   before(:each) do
     @current_user = Factory.create :user
     @controller.stub(:current_user).and_return(@current_user)
-      
-    @exercise = Factory.create :exercise
+    @exercise     = Factory.create :exercise
   end
   
   describe "get show" do
     
     it "assigns the exercise to be displayed to the user" do
-      exercise = mock_model(Exercise)
-      Exercise.stub(:find_by_id).and_return(exercise)
+      Exercise.stub(:find_by_id).and_return(@exercise)
       get 'show'
-      assigns[:exercise].should == exercise
+      assigns[:exercise].should == @exercise
     end
     
     it "renders the show template" do
@@ -26,12 +24,13 @@ describe TutorController do
   describe "post check_syntax" do
     
     before(:each) do
-      @syntax_job = mock_model(SyntaxCheckJob)
-      @syntax_job.stub(:perform)
+      @syntax_job  = mock_model(SyntaxCheckJob, :perform=>true)
       SyntaxCheckJob.stub(:new).and_return(@syntax_job)
       
       @delayed_job = stub_model(Delayed::Job)
       Delayed::Job.stub(:enqueue).and_return(@delayed_job)
+      
+      Exercise.stub(:find).and_return(@exercise)
       
       @code = "int main(){int i = 0; return 0;}"
     end
@@ -42,7 +41,7 @@ describe TutorController do
     end
     
     it "queues a new syntax job" do
-      Exercise.stub(:find).and_return(@exercise)
+
       Delayed::Job.should_receive(:enqueue).with(@syntax_job)
       post :check_syntax, :code=>@code, :id=>@exercise
     end
@@ -63,26 +62,33 @@ describe TutorController do
     end
     
     context "when the given exercise id is not in the database" do
+      before(:each) do
+        Exercise.stub(:find_by_id).and_return(nil)
+      end
+      
       it "does not start a new job" do
         Delayed::Job.should_not_receive(:enqueue).with @syntax_job
-        post :check_syntax, :code=>@code, :id=>'0'
+        post :check_syntax
       end
       
       it "assigns an error message" do
-        post :check_syntax, :id=>'0'
+        post :check_syntax
         assigns[:message].should == 'Aw shoot! And error occured, try again later.'
       end
     end
     
-    context "when the user posts check syntax a few times really fast" do
-      it "checks the database for an existing syntax job" do
+    context "when the user posts check syntax a few times really fast but they already have a syntax job running" do
+      before(:each) do
+        Delayed::Job.stub(:find_by_id).and_return(@delayed_job)
         session[:syntax_check_job_id] = @delayed_job.id
+      end
+      
+      it "checks the database for an existing syntax job" do
         Delayed::Job.should_receive(:find_by_id).with(@delayed_job.id)
         post :check_syntax, :code=>@code, :id=>@exercise
       end
       
       it "doesn't create a new syntax job if the user still has a syntax job in the db" do
-        Delayed::Job.stub(:find_by_id).and_return(@delayed_job)
         Delayed::Job.should_not_receive(:enqueue)
         post :check_syntax, :code=>@code, :id=>@exercise
       end
@@ -101,7 +107,7 @@ describe TutorController do
       response.should render_template('tutor/syntax_status')
     end
     
-    it "returns the sytntax error message" do
+    it "assigns sytntax check resulting message" do
       syntax_check_result = stub_model(SyntaxCheckResult, :error_message=>'syntax error')
       SyntaxCheckResult.stub(:find).and_return(syntax_check_result)
     
