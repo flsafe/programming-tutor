50d3a2aa307004fcc56878a17e222364d22f4d32
Francisco Licea <francisco.licea@gmail.com>
2010-05-23 17:01:35 -0600

    Align assignment

--- app/controllers/school_controller.rb
+++ app/controllers/school_controller.rb
@@ -1,6 +1,6 @@
 class SchoolController < ApplicationController
   def index
     @welcome_message = "Hey there #{current_user.username}! To start things off"
-    @exercise_sets = ExerciseSet.recommend(current_user.id, 2)
+    @exercise_sets   = ExerciseSet.recommend(current_user.id, 2)
   end
 end
