a3f17646428ac76cd4d91c19568a0cafce7d6c5a
Francisco Licea <francisco.licea@gmail.com>
2010-05-23 18:44:43 -0600

    add empty admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.login 'login', :controller=>'user_sessions', :action=>'new'
   map.logout 'logout', :controller=>'user_sessions', :action=>'destroy'
+  map.admin 'admin', :controller=>'admin'
   
   map.resources :user_sessions
 
