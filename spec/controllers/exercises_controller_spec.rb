cd9ef883c1a6c9f43e7385794879b6a855220fec
Francisco Licea <francisco.licea@gmail.com>
2010-05-26 12:38:09 -0600

    Move 'controller for admin owned resource' shared example so that it is visibile from other example groups

--- spec/controllers/exercises_controller_spec.rb
+++ spec/controllers/exercises_controller_spec.rb
@@ -1,6 +1,53 @@
 require 'spec_helper'
 
+shared_examples_for "controller for admin resource" do
+  
+  describe "the current user is not an admin" do
+    before(:each) do
+      current_user = stub_model(User, 'has_role?'=>false, 'is_admin?'=>false)
+      controller.stub(:current_user).and_return(current_user)
+    end
+    
+    it "allows access to the index" do
+      get :index
+      response.should render_template 'index'
+    end
+    
+    it "renders the login page if new is requested" do
+      get :new
+      response.should redirect_to login_path
+    end
+    
+    it "renders the login page if create is posted" do
+      post :create
+      response.should redirect_to login_path
+    end
+    
+    it "renders the login page if edit is requested" do
+      get :edit
+      response.should redirect_to login_path
+    end
+    
+    it "renders the login page if put" do
+      put :update
+      response.should redirect_to login_path
+    end
+    
+    it "renders the login page if delete" do
+      delete  :delete, :id=>'1'
+      response.should redirect_to login_path
+    end
+    
+    it "flashes the permssions message"do
+      get :new
+      flash[:error].should == "You don't have permission to do that!"
+    end
+  end
+end
+
 describe ExercisesController do
+  
+  it_should_behave_like "controller for admin resource"
   
   before(:each) do
     controller.stub(:current_user).and_return(stub_model(User, 'has_role?'=>true))
@@ -20,53 +67,6 @@ describe ExercisesController do
   def stub_exercise_set(stubs = {})
     @stub_exercise_set ||= stub_model(ExerciseSet, stubs).as_null_object
   end
-  
-  shared_examples_for "controller_for_admin_resource" do
-    
-    describe "the current user is not an admin" do
-      before(:each) do
-        current_user = stub_model(User, 'has_role?'=>false)
-        controller.stub(:current_user).and_return(current_user)
-      end
-      
-      it "allows access to the index" do
-        get :index
-        response.should render_template 'index'
-      end
-      
-      it "renders the login page if new is requested" do
-        get :new
-        response.should redirect_to login_path
-      end
-      
-      it "renders the login page if create is posted" do
-        post :create
-        response.should redirect_to login_path
-      end
-      
-      it "renders the login page if edit is requested" do
-        get :edit
-        response.should redirect_to login_path
-      end
-      
-      it "renders the login page if put" do
-        put :update
-        response.should redirect_to login_path
-      end
-      
-      it "renders the login page if delete" do
-        delete  :delete, :id=>'1'
-        response.should redirect_to login_path
-      end
-      
-      it "flashes the permssions message"do
-        get :new
-        flash[:error].should == "You don't have permission to do that!"
-      end
-    end
-  end
-  
-  it_should_behave_like "controller_for_admin_resource"
   
   describe 'get new' do
     
