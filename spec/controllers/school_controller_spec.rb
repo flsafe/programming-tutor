53e3b7e0758aac861999db75daa32edee684c50c
Francisco Licea <francisco.licea@gmail.com>
2010-05-26 12:57:24 -0600

    Clean up the school controller spec. It has some wierd specs

--- spec/controllers/school_controller_spec.rb
+++ spec/controllers/school_controller_spec.rb
@@ -2,26 +2,24 @@ require 'spec_helper'
 
 describe SchoolController, "GET index" do
   
-  context 'a newly registered user browses their home page' do
+  context 'GET index a newly registered user browses their home page' do
+    
+    def current_user(stubs={})
+      @current_user ||= stub_model(User, :username=>'frank')
+    end
+    
+    before(:each) do
+      controller.stub(:current_user).and_return(current_user)
+    end
+    
     it 'gets exercise sets for the user' do
-      current_user = stub_model(User, :username=>'frank', :id=>1000)
-      @controller.stub(:current_user).and_return(current_user)
-      
-      ll_set   = []
-      ll_set << stub_model(ExerciseSet, :title=>'Linked List')
-      ll_set << stub_model(ExerciseSet, :title=>'Linked List 2')
-      
-      h_set = []
-      h_set << stub_model(ExerciseSet, :title=>'Hash')
-      h_set << stub_model(ExerciseSet, :title=>'Hash2')
-      
-      sets = [ll_set, h_set]
-
+      sets = [stub_model(ExerciseSet)]
       ExerciseSet.should_receive(:recommend).with(current_user.id, 2).and_return(sets)
-      
       get :index
-      
-      assigns[:exercise_sets].should   == sets
+    end
+    
+    it 'assigns a welcome message' do
+      get :index
       assigns[:welcome_message].should contain('To start things off')
     end
   end
