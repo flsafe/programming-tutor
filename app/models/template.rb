884a03fb91e6c21c7102c7c00b37c2bcddd802f3
Francisco Licea <francisco.licea@gmail.com>
2010-06-05 13:32:59 -0600

    Add the template model

---
+++ app/models/template.rb
@@ -0,0 +1,32 @@
+class Template < ActiveRecord::Base
+  belongs_to :exercise
+  
+  validates_presence_of :src_language, :src_code
+  
+  def unit_test_file=(unit_test_file)
+    self.attributes = UnitTest.from_file_field(unit_test_file)
+  end
+  
+  def self.from_file_field(unit_test_field)
+    return unless unit_test_field
+    src_language = UnitTest.language(unit_test_field)
+    src_code     = unit_test_field.read
+    
+    {:src_language=>src_language, :src_code=>src_code}
+  end
+  
+  private
+  
+  def self.language(unit_test_field)
+    case UnitTest.base_part_of(unit_test_field.original_filename)
+      when /\.(c)$/
+        $1
+      when /\.(rb)$/
+        $1
+    end
+  end
+  
+  def self.base_part_of(file_name) 
+    File.basename(file_name).gsub(/[^\w._-]/, '') 
+  end 
+end
