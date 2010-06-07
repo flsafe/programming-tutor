d10e3df10feafc075eff56b1c83193e9966e4092
Francisco Licea <francisco.licea@gmail.com>
2010-06-07 16:21:15 -0600

    Add #perform outline as pending examples

--- spec/lib/grade_solution_job_spec.rb
+++ spec/lib/grade_solution_job_spec.rb
@@ -11,7 +11,7 @@ describe GradeSolutionJob do
   end
   
   def template
-    @template ||= mock_model(SolutionTemplate)
+    @template ||= stub(SolutionTemplate)
   end
   
   def code
@@ -27,6 +27,28 @@ describe GradeSolutionJob do
     it "creates a new solution template with the user code" do
       template.should_receive(:fill_in).with(code)
       GradeSolutionJob.new code, current_user.id, exercise.id
+    end
+    
+    it "checks the template for a syntax error" do
+      pending
+    end
+    
+    it "runs the unit test on the template" do
+      pending
+    end
+    
+    it "creates a grade sheet from the unit test results" do
+      pending
+    end
+    
+    it "it saves a grade_job_result to the database with a success message" do
+      pending
+    end
+    
+    context "the template has a syntax error" do
+      it "Saves a grade_job_result with the message describing the syntax error" do
+        pending
+      end
     end
   end
 end
