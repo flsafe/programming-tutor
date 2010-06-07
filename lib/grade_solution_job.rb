14f5a4010ba74f99a7fb5cb371ff5a583d6329f6
Francisco Licea <francisco.licea@gmail.com>
2010-06-07 11:36:28 -0600

    Add failing controller/tutor/#grade_spec

---
+++ lib/grade_solution_job.rb
@@ -0,0 +1,5 @@
+class GradeSolutionJob 
+  def perform
+    
+  end
+end
--- spec/controllers/tutor_controller_spec.rb
+++ spec/controllers/tutor_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe TutorController do
   before(:each) do
     controller.stub(:current_user).and_return(current_user)
+    @code = "int main(){int i = 0; return 0;}"
   end
   
   def current_user(stubs={})
@@ -27,6 +28,17 @@ describe TutorController do
     end
   end
   
+  describe "post grade" do
+    
+    before(:each) do
+      @grade_job = mock_model(GradeSolutionJob, :perform=>true)
+    end
+    
+    it "creates a new grade job for the user, with the given code and the current exercise" do
+      SyntaxCheckJob.should_receive(:new).with(@code, current_user, stub_exercise)
+    end
+  end
+  
   describe "post check_syntax" do
     
     before(:each) do
@@ -37,8 +49,6 @@ describe TutorController do
       Delayed::Job.stub(:enqueue).and_return(@delayed_job)
       
       Exercise.stub(:find).and_return(stub_exercise)
-      
-      @code = "int main(){int i = 0; return 0;}"
     end
     
      it "creates a new syntax job for the user, with the given code and exercise" do
@@ -47,7 +57,6 @@ describe TutorController do
     end
     
     it "queues a new syntax job" do
-
       Delayed::Job.should_receive(:enqueue).with(@syntax_job)
       post :check_syntax, :code=>@code, :id=>stub_exercise
     end
