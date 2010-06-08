6cb4d7c046cb6c39928279acc39158cf2a686b0e
Francisco Licea <francisco.licea@gmail.com>
2010-06-08 15:28:59 -0600

    Cleaned up the SolutionTemplateSpec

--- spec/models/solution_template_spec.rb
+++ spec/models/solution_template_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe SolutionTemplate do
   
+  def template
+    @template ||= stub_model(SolutionTemplate, :src_code=>"void test_function(){<SRC_CODE>}")
+  end
+
   describe "#fill_in" do
     
     it "fills in the source code template with the provided code" do
-      template = SolutionTemplate.new
-      template.src_code = "void test_function(){<SRC_CODE>}"
       template.fill_in('return 0;');
       template.filled_in_src_code.should == "void test_function(){return 0;}"
     end
@@ -14,16 +16,12 @@ describe SolutionTemplate do
   
   describe "#syntax_error" do
     it "returns a syntax error if one is detected in the filled in source code" do
-      template = SolutionTemplate.new
-      template.src_code = "int main(){<SRC_CODE>}"
       template.fill_in('return 0')
       template.syntax_error?.should =~ /error/i
     end
     
     context "the template hasn't been filled in it should return an error" do
       it "does not return a sytnax error" do
-        template = SolutionTemplate.new
-        template.src_code = "int main(){return 0;}"
         template.syntax_error?.should =~ /error/i
       end
     end
