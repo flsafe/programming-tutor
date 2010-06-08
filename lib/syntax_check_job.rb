9e9260b3bc67ffa38c4f8467c42bdc55a9650352
Francisco Licea <francisco.licea@gmail.com>
2010-06-08 15:33:21 -0600

    The syntax_check_job now uses the SyntaxChecker

--- lib/syntax_check_job.rb
+++ lib/syntax_check_job.rb
@@ -3,7 +3,7 @@ class SyntaxCheckJob < Struct.new :code, :user_id, :exercise_id
   attr_accessor :code, :user_id, :exercise_id
   
   def perform
-    error_message = `echo "#{code}" | gcc -x c -fsyntax-only - 2>&1`
+    error_message = SyntaxChecker.syntax_error?(code)
     puts SyntaxCheckResult.delete_all ['user_id = ? AND exercise_id = ?', user_id, exercise_id]
     puts SyntaxCheckResult.create! :user_id=>user_id, :exercise_id=>exercise_id, :error_message=>error_message
   end
