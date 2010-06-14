61f18dde8f08b29fb68577d684905c2d28fe276e
Francisco Licea <francisco.licea@gmail.com>
2010-06-14 17:03:01 -0600

    Fix typo in compiler spec

--- app/models/compiler.rb
+++ app/models/compiler.rb
@@ -1,26 +1,34 @@
 class Compiler
   
   def self.syntax_error?(code)
-    work_dir = APP_CONFIG['work_dir']
-    
-    code_file_path = CozyFileUtils.unique_file_in(work_dir, 'tmp')
-    f = File.open(code_file_path, 'w')
-    f.write(code)
-    f.close
-    
-    out = `gcc -x c -fsyntax-only #{code_file_path} 2>&1`
-    out =~ /error/i ? true : false
+    work_dir       = APP_CONFIG['work_dir']
+    f              = Tempfile.new('syntax', work_dir)
+    code_file_path = f.path
+    begin
+      f.write(code)
+      f.flush
+      out = `gcc -x c -fsyntax-only #{code_file_path} 2>&1`
+      out =~ /error/i ? true : false
+    rescue
+      raise "Could not write tmp scratch file to do the syntax check!"
+    ensure
+      f.close
+    end
   end
 
   def self.compile_to(code, dest_path)
     work_dir       = APP_CONFIG['work_dir']
-    
-    code_file_path = CozyFileUtils.unique_file_in(work_dir, 'tmp')
-    f = File.open(code_file_path, 'w')
-    f.write(code)
-    f.close
-    
-    out = `gcc -x c -o #{dest_path} #{code_file_path} 2>&1`
-    out =~ /error/i ? false : true
+    f              = Tempfile.new('exec', work_dir)
+    code_file_path = f.path
+    begin
+      f.write(code)
+      f.flush
+      out = `gcc -x c -o #{dest_path} #{code_file_path} 2>&1`
+      out =~ /error/i ? false : true
+    rescue
+      raise "Could not write the user's solution code to a tmp scratch file to be compiled!"
+    ensure
+      f.close      
+    end
   end
 end
