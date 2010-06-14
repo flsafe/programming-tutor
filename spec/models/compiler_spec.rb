5ec122aa2df825c1ac73a6d591eb0cdae86f3399
Francisco Licea <francisco.licea@gmail.com>
2010-06-14 17:04:17 -0600

    Use Ruby's Tempfile to generate the temp files in the compiler class

--- spec/models/compiler_spec.rb
+++ spec/models/compiler_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe Compiler do
   
   describe "#syntax_error?" do
-    it "detects a C syntax error" do
+    it "returns false if no syntax error is detected" do
       src = "int main(){return 0;}"
       Compiler.syntax_error?(src).should == false
     end
     
-    it "return true if no syntax error is detected" do
+    it "return true if a syntax error is detected" do
       src = "int main(){int i = 0 return 0;}"
       Compiler.syntax_error?(src).should == true
     end
