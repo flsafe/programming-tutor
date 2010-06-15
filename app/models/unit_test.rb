22e070685e41b2a6950055b1b1b7332418f0fd87
Francisco Licea <francisco.licea@gmail.com>
2010-06-14 18:03:15 -0600

    Move the sanity check for the results of the unit test to a method.

--- app/models/unit_test.rb
+++ app/models/unit_test.rb
@@ -6,7 +6,6 @@ class UnitTest < ActiveRecord::Base
   validates_presence_of :src_language, :src_code
   
   def run_on(template, template_id = nil, solution_code = nil)
-    work_dir          = APP_CONFIG['work_dir']
     user_program_path = CozyFileUtils.unique_file_in(work_dir, 'tmp')
     
     unless Compiler.compile_to(solution_code, user_program_path)
@@ -17,13 +16,12 @@ class UnitTest < ActiveRecord::Base
     unit_test_path     = user_program_path + '-unit-test'
     write_unit_test(unit_test_src_code, unit_test_path)
 
-    results = execute_file(unit_test_path)
-    
+    results      = execute_file(unit_test_path)
     results_hash = YAML.load(results).with_indifferent_access
-    
-    unless results_hash and results_hash['grade']
-      return {'error'=>"A server error occured! Unit test did not return a grade"}
+    if error_results?(results_hash)
+      raise "Then unit test did not return any valid results!"
     end
+   
     results_hash
   end
   
@@ -49,6 +47,18 @@ class UnitTest < ActiveRecord::Base
     f = File.open(path, 'w')
     f.write(src_code)
     f.close
+  end
+  
+  def error_results?(results)
+    #These are exceptional cases that shouldn't happen.
+    #They usually happend when the unit test crashed
+    #or returned some nonsense.
+    case results
+      when !results               then true
+      when results.blank?         then true 
+      when !results[:grade]       then true
+      when results[:grade].blank? then true
+    end
   end
   
    def work_dir
