2ae68e8aee46aa48b0b37c8c77ccaee093b1fc42
Francisco Licea <francisco.licea@gmail.com>
2010-06-28 15:36:00 -0600

    Completed exercises no longer show retake duplicates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ActiveRecord::Base
   acts_as_authentic
   
   has_many :grade_sheets
-  has_many :completed_exercises, :through=>:grade_sheets, :source=>:exercise
+  has_many :completed_exercises, :through=>:grade_sheets, :source=>:exercise, :uniq=>true
   
   has_many :set_grade_sheets
-  has_many :completed_exercise_sets, :through=>:set_grade_sheets, :source=>:exercise_set
+  has_many :completed_exercise_sets, :through=>:set_grade_sheets, :source=>:exercise_set, :uniq=>true
   
   attr_protected :roles_mask
   
