f19c3e60b3b5f7ee0a16c05bf545491a3199e985
Francisco Licea <francisco.licea@gmail.com>
2010-07-01 17:58:06 -0600

    Changed factory#grade_sheet#unit_test_results from a hash to a YAML string

--- spec/factories/grade_sheet.rb
+++ spec/factories/grade_sheet.rb
@@ -2,7 +2,7 @@ Factory.define :grade_sheet do |gs|
   gs.association :user
   gs.association :exercise
   gs.grade 100.0
-  gs.unit_test_results "{'test'=>20}"
+  gs.unit_test_results "grade: 100"
   gs.src_code "int main(){return 0;}"
 end
 
