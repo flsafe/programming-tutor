859f5bdfc5260bba5911396529425126efc378f4
Francisco Licea <francisco.licea@gmail.com>
2010-07-01 17:34:34 -0600

    grade_sheets_controller#show_user_grade_sheet now shows the user grade sheet

--- app/controllers/grade_sheets_controller.rb
+++ app/controllers/grade_sheets_controller.rb
@@ -2,7 +2,7 @@ class GradeSheetsController < ApplicationController
   before_filter :authorize
   
   def show_user_grade_sheet
+    @exercise    = Exercise.find_by_id params[:id]
     @grade_sheet = GradeSheet.find(:first, :conditions=>['exercise_id=? AND user_id=?', params[:id], current_user.id], :order=>'created_at DESC')
-    puts @grade_sheet
   end
 end
--- spec/controllers/grade_sheets_controller_spec.rb
+++ spec/controllers/grade_sheets_controller_spec.rb
@@ -5,22 +5,32 @@ describe GradeSheetsController do
   describe "get show_user_grade_sheet" do
     
     before(:each) do
+      @stub_exercise = stub_model(Exercise)
       @stub_current_user = stub_model(User)
       controller.stub(:current_user).and_return(@stub_current_user)
     end
     
+    it "retrieves the associated exercise" do
+      Exercise.should_receive(:find_by_id).with(@stub_exercise.id.to_s)
+      get :show_user_grade_sheet, :id=>@stub_exercise.id
+    end
+    
+    it "assigns the associated exercise" do
+      Exercise.stub(:find_by_id).and_return(@stub_exercise)
+      get :show_user_grade_sheet
+      assigns[:exercise].should == @stub_exercise
+    end
+    
     it "retrevies the lastest grade sheet for the given exercise_id" do
-      @stub_exercise = stub_model(Exercise)
       GradeSheet.should_receive(:find).with(:first, :conditions=>['exercise_id=? AND user_id=?', @stub_exercise.id.to_s, @stub_current_user.id], :order=>'created_at DESC')
       get :show_user_grade_sheet, :id=>@stub_exercise.id
     end
     
     it "assigns the grade sheet to be displayed" do
-      stub_exercise      = stub_model(Exercise)
       stub_grade_sheet   = stub_model(GradeSheet)
       GradeSheet.stub(:find).and_return(stub_grade_sheet)
       
-      get :show_user_grade_sheet, :id=>stub_exercise.id
+      get :show_user_grade_sheet, :id=>@stub_exercise.id
       assigns[:grade_sheet].should == stub_grade_sheet
     end
   end
