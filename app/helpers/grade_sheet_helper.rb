176d6399e5f7f7e8647615479e167de625da9d80
Francisco Licea <francisco.licea@gmail.com>
2010-07-02 01:21:35 -0600

    Move result validation out of the _grade_sheet partial, remove the grade sheet helper

--- app/helpers/grade_sheet_helper.rb
+++
@@ -1,6 +0,0 @@
-module GradeSheetHelper
-  
-  def valid_result?(results)
-    true if results and results[:grade] and results[:tests]
-  end
-end
--- spec/factories/grade_sheet.rb
+++ spec/factories/grade_sheet.rb
@@ -1,8 +1,17 @@
+unit_test_results =<<DOC
+grade: 100
+tests: 
+  test1:
+    expected:1
+    got:1
+    points:20
+DOC
+
 Factory.define :grade_sheet do |gs|
   gs.association :user
   gs.association :exercise
   gs.grade 100.0
-  gs.unit_test_results "grade: 100"
+  gs.unit_test_results unit_test_results
   gs.src_code "int main(){return 0;}"
 end
 
