8f53711d4d048a799146871077fa73809570df66
Francisco Licea <francisco.licea@gmail.com>
2010-07-19 20:00:29 -0600

    Increase capybara wait time

--- features/support/env.rb
+++ features/support/env.rb
@@ -23,7 +23,7 @@ require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links wi
 # steps to use the XPath syntax.
 Capybara.default_selector = :css
 
-Capybara.default_wait_time = 18
+Capybara.default_wait_time = 20
 
 # If you set this to false, any error raised from within your app will bubble 
 # up to your step definition and out to cucumber unless you catch it somewhere
