522c198506a022b0aad3d890e01a09d1727dfd56
Francisco Licea <francisco.licea@gmail.com>
2010-07-26 15:40:53 -0600

    Renamed progress .gifs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,3 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
-  def spinner
-    image_tag 'loading.gif', :id=>'spinner', :style=>"display:none;"
-  end
-
 end
