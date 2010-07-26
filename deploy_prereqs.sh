112e48cebccaec36d7e1c0e866d43cc878359cd1
Francisco Licea <francisco.licea@gmail.com>
2010-07-26 15:43:48 -0600

    Add preq script

---
+++ deploy_prereqs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+check_if_installed(){
+    type -P $1 &>/dev/null || { echo "$1 is required but not installed!.  Install it first!" >&2; return 1; }
+}
+
+# Used by capistrano to 
+# deploy new instances
+check_if_installed git
+
+# We are using Ruby on Rails
+check_if_installed ruby
+
+# User solutions must run in SELinux
+# sandbox policy. Fedora 13 has one.
+check_if_installed sandbox
+
+# Dependencies installed view gem
+check_if_installed gem
+
+# We use ruby on rails
+check_if_installed rails
+
+# Gem dependencies by bundler
+check_if_installed bundle
+
+# User solutions are compiled
+# with a C compiler.
+check_if_installed gcc
+
+# Deploy via apache
+check_if_installed apachectl
+
+# Database
+check_if_installed mysql
+
+# We use REE-1.8.7, easy to
+# install with rvm. 
+check_if_installed rvm
+
+# Used to kill delayed_job daemon
+# when deploying.
+check_if_installed pidof
+check_if_installed kill
