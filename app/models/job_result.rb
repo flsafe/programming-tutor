858e2845553bbcec2e8d2d27e9d2a9e3f5c91f31
Francisco Licea <francisco.licea@gmail.com>
2010-08-28 21:53:13 -0500

    Add passing specs for JobResult

--- app/models/job_result.rb
+++ app/models/job_result.rb
@@ -1,2 +1,23 @@
 class JobResult < ActiveRecord::Base
+  
+  validates_presence_of :user_id, :exercise_id
+  
+  def self.place_result(result)
+    JobResult.clear_slot(result[:user_id], result[:exercise_id])
+    JobResult.create result
+  end
+  
+  def self.pop_result(conds)
+    user_id     = conds[:user_id]
+    exercise_id = conds[:exercise_id]
+    JobResult.clear_slot(user_id, exercise_id)
+    JobResult.find :first, :conditions=>{:user_id=>user_id, :exercise_id=>exercise_id}
+  end
+  
+  protected
+  
+  def self.clear_slot(user_id, exercise_id)
+     JobResult.delete_all(:user_id=>user_id, :exercise_id=>exercise_id)
+  end
+  
 end
--- spec/models/job_result_spec.rb
+++ spec/models/job_result_spec.rb
@@ -5,31 +5,33 @@ describe JobResult do
   before(:each) do
     @stub_user     = stub_model(User)
     @stub_exercise = stub_model(Exercise)
+    @example_result = {:user_id=>@stub_user.id, :exercise_id=>@stub_exercise.id, :data=>'ok', :error_message=>'error'}
   end
   
   describe "#place_result" do
     
-    
     it "clears any job results in the db with the given user_id and exercise_id (clears the slot)" do
-      JobResult.should_receive(:destroy).with(:user_id=>@stub_user.id, :exercise_id=>@stub_exercise.id)
+      JobResult.should_receive(:delete_all).with(:user_id=>@stub_user.id, :exercise_id=>@stub_exercise.id)
       JobResult.place_result(:user_id=>@stub_user.id, :exercise_id=>@stub_exercise.id, :data=>"okay")
     end
     
-    it "inserts a job result object into the db with the given user_id and exercise_id (places a result in the slot)" do
-      
+    it "inserts a job result object into the db with the param info(places a result in the slot)" do
+      JobResult.should_receive(:create).with(@example_result)
+      JobResult.place_result(@example_result)
     end
   end
   
   
   describe "#pop_result" do
     
-  
     it "retrieves a job result object with the given user_id and exercise_id" do
-      
+      JobResult.should_receive(:find).with(:first, :conditions=>{:user_id=>@stub_user.id, :exercise_id=>@stub_exercise.id})
+      JobResult.pop_result(:user_id=>@stub_user.id, :exercise_id=>@stub_exercise.id)
     end
     
     it "deletes the record from the database (removes the result from the slot)" do
-      
+      JobResult.should_receive(:delete_all).with(:user_id=>@stub_user.id, :exercise_id=>@stub_exercise.id)
+      JobResult.place_result(@example_result)
     end
     
   end
