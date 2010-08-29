b6bb2487b84474e96b672fcd1be350b1d1765ec4
Francisco Licea <francisco.licea@gmail.com>
2010-08-28 22:24:53 -0500

    Add pop_result function to syntax_check_job

--- lib/syntax_check_job.rb
+++ lib/syntax_check_job.rb
@@ -4,7 +4,22 @@ class SyntaxCheckJob < Struct.new :code, :user_id, :exercise_id
   
   def perform
     error_message = Compiler.check_syntax(code)
-    puts SyntaxCheckResult.delete_all ['user_id = ? AND exercise_id = ?', user_id, exercise_id]
-    puts SyntaxCheckResult.create! :user_id=>user_id, :exercise_id=>exercise_id, :error_message=>error_message
+    SyntaxCheckResult.delete_all ['user_id = ? AND exercise_id = ?', user_id, exercise_id]
+    SyntaxCheckResult.create! :user_id=>user_id, :exercise_id=>exercise_id, :error_message=>error_message
+    JobResult.place_result :user_id=>user_id, :exercise_id=>exercise_id, :data=>error_message
+  end
+  
+  def pop_result(conds)
+    user_id, exercise_id = conds[:user_id], conds[:exercise_id]
+    result = JobResult.pop_result(:user_id=>user_id, :exercise_id=>exercise_id)
+    if result
+      syntax_error?(result) 
+    end
+  end
+  
+  protected 
+  
+  def syntax_error?(result)
+    result.data =~ /syntax\s+error/i ? result.data : 'No syntax error detected!'
   end
 end
