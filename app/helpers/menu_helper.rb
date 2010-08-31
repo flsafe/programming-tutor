130e1e8a616363eadf28576991c49eff38c8dd89
Francisco Licea <francisco.licea@gmail.com>
2010-08-31 11:36:00 -0500

    fixed the navigation menu

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,15 +1,18 @@
 module MenuHelper
-  def black_list
-    @black_list ||= [/^\/$/, 
-                     /\/login/, 
-                     /\logout/, 
-                     /\/users\/new/, 
-                     /user_sessions/, 
-                     /tutor\/show/, 
-                     /tutor\/grade/]
-  end
 
   def show_menu?
-    not black_list.detect {|no_show| request.request_uri =~ no_show }
+    @url_black_list    = [ /^\/$/]
+    
+    @action_black_list = ['user_sessions:new',
+                          'user_sessions:create',
+                          'users:new',
+                          'users:create']
+    
+    current_action        = "#{controller.controller_name}:#{controller.action_name}"
+    not_black_list_url    = !(@url_black_list.detect {|no_show| request.request_uri =~ no_show } )
+    not_black_list_action = !(@action_black_list.detect {|no_show| current_action == no_show} )
+    
+    not_black_list_url and not_black_list_action
   end
+  
 end
