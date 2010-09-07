a161b9611372b649ab09c173ebbaeedf70f283be
Francisco Licea <francisco.licea@gmail.com>
2010-09-07 12:14:54 -0500

    Slightly more sophisticated view all grades sheets feature

--- app/controllers/grade_sheets_controller.rb
+++ app/controllers/grade_sheets_controller.rb
@@ -1,6 +1,10 @@
 class GradeSheetsController < ApplicationController
   before_filter :authorize
   
+  def show
+    @grade_sheet = GradeSheet.find(params[:id])
+  end
+  
   def show_user_grade_sheet
     @exercise    = Exercise.find(params[:id])
     @grade_sheet = GradeSheet.find(:first, :conditions=>['exercise_id=? AND user_id=?', @exercise.id, current_user.id], :order=>'created_at DESC')
@@ -8,7 +12,7 @@ class GradeSheetsController < ApplicationController
   
   def show_all
     @exercise     = Exercise.find(params[:id])
-    @grade_sheets = GradeSheet.find(:all, :conditions=>['exercise_id=?', @exercise.id], :order=>'grade DESC')
+    @grade_sheets = GradeSheet.find(:all, :conditions=>['exercise_id=?', @exercise.id], :order=>'created_at DESC', :group=>'username', :joins=>:user)
   end
   
 end
