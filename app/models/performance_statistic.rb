b6ce254a71b7d15f677ca8db45b785c2a965579c
Francisco Licea <francisco.licea@gmail.com>
2010-09-13 14:57:45 -0500

    Be more specific about which of the latest stats to get wit the exercise_id

--- app/models/performance_statistic.rb
+++ app/models/performance_statistic.rb
@@ -3,7 +3,7 @@ class PerformanceStatistic < ActiveRecord::Base
   validates_presence_of :user_id, :exercise_id, :name, :value
   
   def self.place_stat(user_id, exercise_id, stat_name, value)
-    PerformanceStatistic.clear_slot(user_id, stat_name) #We assume the user can only do one exercise at a time, so clear the stats from the last exercise that was completed
+    PerformanceStatistic.clear_slot(user_id, exercise_id, stat_name) #We assume the user can only do one exercise at a time, so clear the stats from the last exercise that was completed
     PerformanceStatistic.create! :user_id=>user_id, :exercise_id=>exercise_id, :name=>stat_name, :value=>value
   end
   
@@ -14,8 +14,8 @@ class PerformanceStatistic < ActiveRecord::Base
   
   protected
   
-  def self.clear_slot(user_id, stat_name)
-    PerformanceStatistic.delete_all :user_id=>user_id, :name=>stat_name
+  def self.clear_slot(user_id, exercise_id, stat_name)
+    PerformanceStatistic.delete_all :user_id=>user_id, :exercise_id=>exercise_id, :name=>stat_name
   end
   
 end
