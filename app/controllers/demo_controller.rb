96528412393f34af78d51d36e1f8926568371569
Francisco Licea <francisco.licea@gmail.com>
2010-09-22 09:35:29 -0500

    Try-It now goes to a simple demo page.

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,7 +1,19 @@
 class DemoController < ApplicationController
   
   def index
-    
+    @exercises = get_demo_exercises
   end
   
+  protected
+  
+  def get_demo_exercises
+    demo_exercise_titles = APP_CONFIG['demo_exercise_titles']
+    @demo_exercises = find_exercise_by_titles(demo_exercise_titles)
+  end
+  
+  def find_exercise_by_titles(demo_exercise_titles)
+    exercises = Exercise.find :all, :conditions=>{:title=>demo_exercise_titles}
+    raise 'Could not find all the demo exercises' if exercises.size != demo_exercise_titles.size
+    exercises
+  end
 end
