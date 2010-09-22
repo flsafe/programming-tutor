25112bbc760add08c615939b7e2d9d528feb7fde
Francisco Licea <francisco.licea@gmail.com>
2010-09-22 09:48:50 -0500

    App-nav is not shown in demo/index

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -4,9 +4,10 @@ module MenuHelper
     @url_black_list    ||= [ /^\/$/]
     
     @action_black_list ||= ['user_sessions:new',
-                          'user_sessions:create',
-                          'users:new',
-                          'users:create']
+                            'user_sessions:create',
+                            'users:new',
+                            'users:create',
+                            'demo:index']
     
     current_action        = "#{controller.controller_name}:#{controller.action_name}"
     not_black_list_url    = !(@url_black_list.detect {|no_show| request.request_uri =~ no_show } )
