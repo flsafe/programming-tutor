e9f1ae2d0cbf4cca790bce74d9e0c42d0917e4d9
Francisco Licea <francisco.licea@gmail.com>
2010-09-23 10:05:32 -0500

    remove set_grade_sheets from users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,6 @@ class User < ActiveRecord::Base
   has_many :grade_sheets, :dependent=>:destroy
   has_many :completed_exercises, :through=>:grade_sheets, :source=>:exercise, :uniq=>true
   
-  has_many :set_grade_sheets, :dependent=>:destroy
-  has_many :completed_exercise_sets, :through=>:set_grade_sheets, :source=>:exercise_set, :uniq=>true
-  
   attr_protected :roles_mask, :anonymous
   
   def grade_for?(exercise)
