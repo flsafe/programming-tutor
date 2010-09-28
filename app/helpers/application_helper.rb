c13a627edb037c3ecac2063648f5f06f3f7fbad4
Francisco Licea <francisco.licea@gmail.com>
2010-09-28 09:45:14 -0500

    Add helper method :current_user_home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def current_user_home
+    return if not current_user
+    
+    if current_user.anonymous?
+      link_to 'Home', root_url
+    else
+      link_to 'Home', :controller=>'overview'
+    end
+  end
+  
 end
