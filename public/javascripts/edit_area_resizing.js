7960fa35e52b934478d988b255d5b8b256360903
Francisco Licea <francisco.licea@gmail.com>
2010-10-06 19:18:21 -0500

    cleanup

--- public/javascripts/edit_area_resizing.js
+++ public/javascripts/edit_area_resizing.js
@@ -14,21 +14,18 @@ function unfocusEditArea(){
 
 function resizeEditAreaDimensions(){
   width_offset = -20
-  height_offset = -155
 
   edit_area = document.getElementById('textarea_1')
-  edit_area.style.width = oneThirdWindowWidth() * 2 + width_offset + 'px'
-  height = window.innerHeight
-  edit_area.style.height = height + height_offset + 'px'
+  edit_area.style.width = twoThirdsWindowWidth(width_offset) 
+  edit_area.style.height = calcHeight() 
 }
 
 function resizeExerciseTextDimensions(){
   width_offset = -25
-  height_offset = -155
 
   exercise_text = document.getElementById('exercise_problem_text')
-  exercise_text.style.width = oneThirdWindowWidth() + width_offset + 'px'
-  exercise_text.style.height = window.innerHeight + height_offset + 'px'
+  exercise_text.style.width = oneThirdWindowWidth(width_offset) 
+  exercise_text.style.height = calcHeight(); 
 }
 
 function flashEditArea(){
@@ -36,9 +33,12 @@ function flashEditArea(){
   editAreaLoader.show('textarea_1')
 }
 
-function oneThirdWindowWidth(){
+function twoThirdsWindowWidth(offset){
+  return ((window.innerWidth / 3) * 2) + offset + 'px'
+}
+function oneThirdWindowWidth(offset){
   width = window.innerWidth
-  return width / 3
+  return (width / 3) + offset + 'px'
 }
 
 function calcHeight(){
