0011d3bfa0752051db6b1d315223f8891d09a1e6
Francisco Licea <francisco.licea@gmail.com>
2010-10-07 13:11:25 -0500

    style

--- public/javascripts/edit_area_setup.js
+++ public/javascripts/edit_area_setup.js
@@ -2,8 +2,12 @@
 editAreaLoader.init({
   id : "textarea_1"		       
   ,syntax: "c"			         
-  ,start_highlight: true		 
-  ,allow_toggle:  true
+  ,start_highlight: false 
+  ,allow_toggle:  false
   ,toolbar: "undo, redo, help"
   ,replace_tab_by_spaces: 2
+  , allow_resize: 'no'
+  , gecko_spellcheck: false
+  , min_width: 50
+  , min_height: 50
 });
