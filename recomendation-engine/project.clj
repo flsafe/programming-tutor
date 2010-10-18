1eb8c47827ca8f01bc99a104718bebe9b3e4a3d5
Francisco Licea <francisco.licea@gmail.com>
2010-10-18 11:13:51 -0500

    Add initial recomendation engine

---
+++ recomendation-engine/project.clj
@@ -0,0 +1,5 @@
+(defproject recomendation-engine "1.0.0-SNAPSHOT"
+  :description "Recomends exercises to users"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[org.clojars.gfodor/lein-nailgun "1.1.0"]])
