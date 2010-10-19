240a70973eeed9a368f84952040a04c550a64cb0
Francisco Licea <francisco.licea@gmail.com>
2010-10-19 09:48:40 -0500

    Add basic sqlite connectivity code

---
+++ recomendation-engine/src/recomendation_engine/database.clj
@@ -0,0 +1,11 @@
+(ns recomendation-engine.database
+  (:use [clojure.contrib.sql :only (with-connection with-query-results)])
+  (:import (java.sql DriverManager)))
+
+(def db {:classname "org.sqlite.JDBC"
+         :subprotocol "sqlite"
+         :subname "../db/development.sqlite3"})
+
+(defn print-in-db []
+  (with-query-results res ["select * from users"]
+    res))
