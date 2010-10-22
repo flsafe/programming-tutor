69f38df02758c1cd6319e23e7eb0d9a74b51d907
Francisco Licea <francisco.licea@gmail.com>
2010-10-21 23:11:13 -0500

    refactor ratings controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,12 +3,17 @@ class RatingsController < ApplicationController
   before_filter :require_user
 
   def create
-    rating = APP_CONFIG['rating_values'][params['rating']]
-
     Rating.create!(:user_id=>current_user.id, 
                    :exercise_id=>params[:exercise_id],
-                   :rating=>rating)
+                   :rating=>rating_value)
 
     render :text=>""
+  end
+
+  protected
+
+  def rating_value
+    rating_str = params[:rating].sub(/\-/, '_')
+    rating = APP_CONFIG['rating_values'][rating_str]
   end
 end
