e84176f32b58021b538303a044a9c493fdebaf7b
Francisco Licea <francisco.licea@gmail.com>
2010-10-27 13:22:33 -0500

    Add yaml parse to lein deps

--- recomendation-engine/project.clj
+++ recomendation-engine/project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring/ring-core "0.3.2"]
-                 [ring/ring-jetty-adapter "0.3.2"]]
+                 [ring/ring-jetty-adapter "0.3.2"]
+                 [clj-yaml "0.3.0"]]
 
   :dev-dependencies [[org.clojars.gfodor/lein-nailgun "1.1.0"]
                      [sqlitejdbc "0.5.6"]
