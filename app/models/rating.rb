e0511989129dc1f19b1fe3634256674239cef62a
Francisco Licea <francisco.licea@gmail.com>
2010-10-26 22:42:10 -0500

    Rating model pushes rating via http post after save, add server uri to config.yml

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,10 +1,29 @@
+require 'net/http'
+
 class Rating < ActiveRecord::Base
   validates_presence_of :user_id, :exercise_id, :rating
   validates_uniqueness_of :exercise_id, :scope=>[:user_id]
+
+  after_save :push_rating
 
   def self.to_value(rating_str)
     rating_str = rating_str.sub(/\-/, '_')
     rating = APP_CONFIG['rating_values'][rating_str]
     rating.to_f
+  end
+
+  def push_rating
+    Thread.new(user_id, exercise_id, rating) do |user_id, exercise_id, rating|
+      uri = APP_CONFIG['recomendation_server_uri'][Rails.env]
+      begin 
+        Net::HTTP.post_form(URI.parse(uri),
+                            "user_id"=>user_id, 
+                            "exercise_id"=>exercise_id,
+                            "rating"=>rating)
+      rescue Exception => e
+        #nothing for now
+        puts "No rec server connection"
+      end
+    end
   end
 end
