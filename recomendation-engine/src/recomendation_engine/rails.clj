65e669b74ee15c6aeb9086dfb5da170a3b91ac9e
Francisco Licea <francisco.licea@gmail.com>
2010-10-27 19:41:23 -0500

    Add db-to-jdbc function that converts rails db connection info to jdbc

--- recomendation-engine/src/recomendation_engine/rails.clj
+++ recomendation-engine/src/recomendation_engine/rails.clj
@@ -1,8 +1,53 @@
+(ns recomendation-engine.rails
+  (:require [clj-yaml.core :as yaml]))
 
-(def rails-env
+(def env
   (or (keyword (System/getenv "RAILS_ENV")) :development)) 
 
-(def rails-db-config
+(defn db 
+  [rails-env]
   (rails-env
     (yaml/parse-string 
       (slurp "../config/database.yml"))))
+
+(defn subprotocol
+  "Return a map with :subprotocol mapping
+  to the rails db :adapter"
+  [rails-db-config]
+  (:adapter rails-db-config))
+
+(defn proj-relative
+  "Returns true if the database name is
+   a file relative to the rails root dir"
+  [rails-db-config]
+  (not= 
+    -1 
+    (.indexOf (:adapter rails-db-config) "sqlite")))
+
+(defn subname
+  "Return a map with subname mapping to
+   //host/database"
+  [rails-db-config]
+  (if (proj-relative rails-db-config)
+    (str "//"  "/.." "/" (:database rails-db-config))
+    (str "//" (:host rails-db-config) "/" (:database rails-db-config))))
+
+(defn classname
+  "Return a map with :classname mapping to the jdbc class to load"
+  [rails-db-config]
+  (let [adapter-to-class {"sqlite3"    "org.sqlite.JDBC"
+                          "mysql"      "com.mysql.jdbc.Driver"
+                          "postgresql" "org.postgresql.Driver"}]
+    (adapter-to-class (:adapter rails-db-config))))
+
+(defn db-to-jdbc 
+  "Returns a db map with db credentials from 
+   rails-project/config/database.yml suitable for use with
+   clojure.contirb.sql/with-connection"
+  [db-config rails-env]
+   {:classname   (classname   (db rails-env))
+    :subprotocol (subprotocol (db rails-env))
+    :subname     (subname     (db rails-env))
+    :user        (:username db-config)
+    :password    (:password db-config)})
+
