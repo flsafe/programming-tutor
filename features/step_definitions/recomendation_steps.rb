017e43bc95d4cfea6091226b02a6179ed56f9c19
Francisco Licea <francisco.licea@gmail.com>
2010-11-01 22:53:36 -0500

    Remove output prints, fix typo, use include? instead of detect

--- features/step_definitions/recomendation_steps.rb
+++ features/step_definitions/recomendation_steps.rb
@@ -3,11 +3,8 @@ Then /^the recomendation for "([^\"]*)" should be "([^\"]*)"$/ do |username, exe
   exercise = Exercise.find_by_title(exercise_title)
 
   recomendations = Recomendation.for(user.id)
-  has_exercise = recomendations.detect {|ex_id| ex_id == exercise.id}
+  has_exercise = recomendations.include? exercise.id
 
-  puts "*" * 10
-  puts recomendations
-    
   has_exercise.should == true
-  recomendationsize.should == 1
+  recomendations.size.should == 1
 end
