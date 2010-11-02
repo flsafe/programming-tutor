d1e4cc1b8a7d9b4c1b45e8ea5505900531e60e7b
Francisco Licea <francisco.licea@gmail.com>
2010-11-02 14:00:07 -0500

    Make a more specific test case for the rails.clj. Test both mysql and sqlite configs

--- recomendation-engine/test/recomendation_engine/test/rails.clj
+++ recomendation-engine/test/recomendation_engine/test/rails.clj
@@ -2,12 +2,12 @@
   (:require [recomendation-engine.rails :as rails])
   (:use [clojure.test :only (deftest is)])) 
 
-(def rails-dev-db-config
+(def rails-dev-db-config-sqlite
   {:adapter  "sqlite3"
    :database "db/development.sqlite3"
    :timeout  5000})
 
-(def expected-dev-jdbc-config
+(def expected-dev-jdbc-config-sqlite
   {:classname   "org.sqlite.JDBC"
    :subprotocol "sqlite"
    :subname     "../db/development.sqlite3"
@@ -29,8 +29,8 @@
    :user        "blueberrytree"
    :password    "passpass"})
 
-(deftest test-dev-config
+(deftest test-dev-config-sqlite
    (is 
      (= 
-       (rails/db-to-jdbc rails-dev-db-config :development)
-        expected-dev-jdbc-config)))
+       (rails/db-to-jdbc rails-dev-db-config-sqlite :development)
+        expected-dev-jdbc-config-sqlite)))
