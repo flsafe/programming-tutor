600e52e41994ed1f0bc81e1ce9d2e9c7710d43e3
Francisco Licea <francisco.licea@gmail.com>
2010-11-05 17:11:09 -0500

    Fix bug in exercise_recomendation_list where the list can't be mapped out

--- app/models/exercise_recomendation_list.rb
+++ app/models/exercise_recomendation_list.rb
@@ -3,6 +3,11 @@ class ExerciseRecomendationList
   attr_reader :list
 
   def initialize(recomendation_list_as_string)
+    pp recomendation_list_as_string
     @list = recomendation_list_as_string.split(/,/).map {|str_id| str_id.to_i}
+  end
+
+  def exercise_recomendation_list
+    @list.join(',')
   end
 end
