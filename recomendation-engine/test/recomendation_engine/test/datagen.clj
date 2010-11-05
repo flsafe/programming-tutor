498a8d0b46fd84878bccc1f7eaeb5d6e577cbcd1
Francisco Licea <francisco.licea@gmail.com>
2010-11-05 00:34:07 -0500

    rand-prefs now deltes half the ratings for the person specified

--- recomendation-engine/test/recomendation_engine/test/datagen.clj
+++ recomendation-engine/test/recomendation_engine/test/datagen.clj
@@ -11,10 +11,18 @@
         item (range 1 (inc num-items))]
     {:u person :i item :r (next-normal-rand 5 7)}))
 
-(defn rand-prefs [num-people num-items]
-  (reduce #(assoc-in %1 [(:u %2)]
-                     (assoc-in (get %1 (:u %2) {}) [(:i %2)] (:r %2))) 
-          {{}}
-          (rand-ratings num-people num-items)))
+(defn take-half [person prefs]
+  (let [remove-keys (range 1
+                           (/
+                             (inc (count (prefs person)))
+                             2))]
+    (apply dissoc
+           (prefs person)
+           remove-keys)))
 
-
+(defn rand-prefs [person num-people num-items]
+  (let [prefs (reduce #(assoc-in %1 [(:u %2)]
+                      (assoc-in (get %1 (:u %2) {}) [(:i %2)] (:r %2))) 
+                      {{}}
+                      (rand-ratings num-people num-items))]
+    (assoc prefs person (take-half person prefs))))
