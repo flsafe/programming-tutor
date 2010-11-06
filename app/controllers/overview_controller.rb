d54ac2e244e25e51f82ca3605a16601d40ee1b04
Francisco Licea <francisco.licea@gmail.com>
2010-11-06 14:30:06 -0500

    Whitespace

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -3,6 +3,6 @@ class OverviewController < ApplicationController
   before_filter :require_user
   
   def index
-    @exercises   = Recomendation.for(current_user.id) 
+    @exercises = Recomendation.for(current_user.id)
   end
 end
