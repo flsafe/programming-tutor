0a6c58141d28f3382d297c23279525c910f05cb7
Francisco Licea <francisco.licea@gmail.com>
2010-12-03 01:11:11 -0600

    Modify recomendation server and rec server startup scripts to use the uri described int conf/config.yml depending on the Rails.env

--- recomendation-engine/src/recomendation_engine/server.clj
+++ recomendation-engine/src/recomendation_engine/server.clj
@@ -1,7 +1,9 @@
 (ns recomendation-engine.server
   (:use ring.adapter.jetty)
   (:use ring.middleware.params)
-  (:use recomendation-engine.app))
+  (:use recomendation-engine.app)
+  (:require [recomendation-engine.rails :as rails])
+  (:require [clj-yaml.core :as yaml]))
 
 (defn parse-integer [str]
   (try (Integer/parseInt str) 
@@ -35,7 +37,23 @@
 (def app
   (-> #'handler
     (wrap-params)))
-    
+
+(def server-uris 
+  (:recomendation_server_uri
+    (yaml/parse-string
+      (slurp "../config/config.yml"))))
+
+(defn get-port [uri]
+  (or 
+    ((first (re-seq #"^\w+://[^/:]+:?(\d+)?/?" uri)) 1)
+    ; By convention if no port is specified we use the development port
+    ; 8083
+    "8083"))
+
+(defn env-port [env-uri-map]
+  (get-port
+    (get env-uri-map rails/env)))
+
 (defn boot-server []
-  (run-jetty app {:port 8080}))
+  (run-jetty app {:port (parse-integer (env-port server-uris))}))
 
