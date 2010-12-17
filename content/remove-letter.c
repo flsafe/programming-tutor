14795ca1c2fbf979fe7f7fefbbd6be7e9d616855
Francisco Licea <francisco.licea@gmail.com>
2010-12-17 00:42:39 -0600

    Use setrlimit in remove-letter.c

--- content/remove-letter.c
+++ content/remove-letter.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
 
 /* When in production mode runing in Fedora Linux,
- * we use SECCOMP to limit what system calls can be made.
+ * we use the SECCOMP to limit system resources.
  * Otherwise we assume we are running on a development box
  * with no need for SECCOMP. */
 
 #ifdef LINUX_SECCOMP
-  #include<sys/prctl.h>
-  #include<linux/prctl.h>
+  #include<sys/time.h>
+  #include<sys/resource.h>
 #endif
 
 #define MAX_STR 255
 
 void remove_char(char, char[]);
-void do_remove_char(char, char[]);
+
+#ifdef LINUX_SECCOMP
+  void set_limit(int resource, int value){         
+    struct rlimit rl;
+                  
+    getrlimit(resource, &rl);
+    rl.rlim_cur = value;
+    setrlimit(resource, &rl);
+  }
+#endif
 
 int main(){
   char str[MAX_STR + 1];
   char rm_char;
 
-  #ifdef LINUX_SECCOMP  
-  prctl(PR_SET_SECCOMP, 1);
+  #ifdef LINUX_SECCOMP 
+    set_limit(RLIMIT_CPU, 4); 
+    set_limit(RLIMIT_NOFILE, 0); 
+    set_limit(RLIMIT_FSIZE, 1); 
+    set_limit(RLIMIT_NPROC, 1);  
+    set_limit(RLIMIT_DATA, 100000); 
+    set_limit(RLIMIT_STACK, 100000); 
+    set_limit(RLIMIT_AS, 100000); 
   #endif
 
   scanf("%c %255s",&rm_char, str);
