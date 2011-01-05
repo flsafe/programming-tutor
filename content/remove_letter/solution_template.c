646f637adcaa5f6192ed391a483fb49e9193859a
Francisco Licea <francisco.licea@gmail.com>
2011-01-05 16:30:24 -0600

    Add initial content/push script with the start of a new exercise

---
+++ content/remove_letter/solution_template.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+
+/* When in production mode runing in Fedora Linux,
+ * we use the SECCOMP flag to limit system resources.
+ * Otherwise we assume we are running on a development box
+ * with no need for SECCOMP. */
+
+#ifdef LINUX_SECCOMP
+  #include<sys/time.h>
+  #include<sys/resource.h>
+#endif
+
+#define MAX_STR 255
+
+#ifdef LINUX_SECCOMP
+  void set_limit(int resource, int value){         
+    struct rlimit rl;
+                  
+    getrlimit(resource, &rl);
+    rl.rlim_cur = value;
+    rl.rlim_max = value;
+    setrlimit(resource, &rl);
+  }
+#endif
+
+void remove_char(char, char[]);
+
+int main(){
+  char str[MAX_STR + 1];
+  char rm_char; 
+  memset(str, '\0', (MAX_STR + 1) * sizeof(char));
+
+  #ifdef LINUX_SECCOMP 
+    set_limit(RLIMIT_CPU, 4); 
+    set_limit(RLIMIT_NOFILE, 0); 
+    set_limit(RLIMIT_FSIZE, 1); 
+    set_limit(RLIMIT_NPROC, 1);  
+    set_limit(RLIMIT_DATA, 100000); 
+    set_limit(RLIMIT_STACK, 100000); 
+    set_limit(RLIMIT_AS, 100000); 
+  #endif
+
+  scanf("%c %255s",&rm_char, str);
+  remove_char(rm_char, str);
+
+  str[MAX_STR] = '\0'; /*Always null terminated*/
+  printf("%s", str);
+  return 0;
+}
+
+/*
+ * This is where the user solution code is inserted. 
+ * The user solution code is run through the ../lib/IncludeScrubber
+ * in the ruby file grade_solution_job.rb
+ * to get rid of any user includes. Therefore it is important to include
+ * any libs that will be needed above
+ */
+
+/*start_prototype*/
+void remove_char(char c, char str[]){
+  
+  /*your code here*/
+  
+}
+/*end_prototype*/
---
+++ content/remove_letter/unit_test.rb
@@ -0,0 +1,90 @@
+require 'yaml'
+
+class RemoveLetterUnitTest
+  
+  attr_accessor :results
+  
+  def initialize()
+    @results = { :tests     => {},
+                 :run_times => {} }
+  end
+  
+  def start
+    test_all_letters_removed
+    test_first_letter_removed
+    test_last_letter_removed
+    test_no_letters_removed
+    test_empty_string
+    
+    results[:grade] = count_points
+  end
+  
+  def count_points
+    tests  = results[:tests]
+    points = 0
+    tests.each_pair do |test_name, test_results|
+      points += test_results[:points]
+    end
+    points
+  end
+  
+  def test_all_letters_removed
+    title = "Remove all letters: remove('c', 'ccc')"
+    run_with_and_expect('c ccc', '', title, 20)
+  end
+  
+  def test_first_letter_removed
+    title = "Remove first letter: remove('c', 'caaa')"
+    run_with_and_expect('c caaa', 'aaa', title, 20)
+  end
+  
+  def test_last_letter_removed
+    title = "Remove last letter: remove('c', 'aaac')"
+    run_with_and_expect('c aaac', 'aaa', title, 20)
+  end
+  
+  def test_no_letters_removed
+    title = "Remove no letters: remove('c', 'aaa')"
+    run_with_and_expect('c aaa', 'aaa', title, 20)
+  end
+  
+  def test_empty_string
+    title = "Remove from empty string: remove('c', '')"
+    run_with_and_expect('c', '', title, 20)
+  end
+  
+  protected
+  
+  def run_with(input)
+    p = IO.popen('-', 'w+')
+    if p 
+      p.write(input)
+      p.close_write
+      
+      result = p.read()
+      p.close
+      result
+    else
+      exec("sandbox ./<EXEC_NAME>")
+    end   
+  end
+
+  def run_with_and_expect(input, expected, title, points)
+   result = run_with(input)
+   if result.strip.chomp == expected.strip.chomp
+     add(:tests, title, :expected=>expected, :got=>result, :points=>points)
+   else
+     add(:tests, title, :expected=>expected, :got=>result, :points=>0)
+   end
+  end
+  
+  def add(category, test_name = '',  info = {})
+    if not test_name == ''
+      results[category][test_name] = info
+    end
+  end
+end
+  
+test = RemoveLetterUnitTest.new
+test.start
+puts YAML.dump(test.results)
