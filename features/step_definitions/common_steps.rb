f404c0f74a09a47bca6c2afee0d307d971f6a2d5
Francisco Licea <francisco.licea@gmail.com>
2011-01-10 15:17:41 -0600

    Add new feature steps for filling oout any exercise is content

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -8,6 +8,21 @@ Given /^I am doing the exercise RemoveChar$/ do
   }
 end
 
+Given /^I have filled in the exercise "([^"]*)" with "([^"]*)$/ do |title, solution_file|
+  title = title.tableize
+  solution_template = File.join(title, 'solution_template.rb')
+  unit_test = File.join(title, 'unit_test.rb')
+  solution = Fille.join(title, solution_file)
+
+  steps %Q{
+    Given I am logged in as the user "frank"
+		And there exists an exercise set "Exercise Set" with "#{title}" and "Ex2"
+		And the exercise "#{title}" has the solution template "#{solution_template}" and the unit test "#{unit_test}"
+		And I am viewing the tutor page for "#{title}"
+		And I fill in the text editor with the solution "#{solution}"
+  }
+end
+
 When /^I wait for "([^\"]*)" seconds$/ do |secs|
  sleep(secs.to_i)
 end
