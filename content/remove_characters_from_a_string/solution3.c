774f148bc9d2dede4d2179315808b4f67604c4db
Francisco Licea <francisco.licea@gmail.com>
2011-01-24 11:27:35 -0600

    Next draft of tutorial

--- content/remove_characters_from_a_string/solution3.c
+++ content/remove_characters_from_a_string/solution3.c
@@ -6,33 +6,20 @@
  * Allocate a buffer to copy str into. Caller
  * frees memory.
  */
-char* allocate_cpy_buffer(char str[]){
+char* allocate_tmp_buffer(char str[]){
   unsigned int len = 0;
-  char *cpy_buffer = NULL;
+  char *tmp_buffer = NULL;
 
   if(! str )
-    return;
+    return NULL;
 
   len = strlen(str) + 1; 
-  cpy_buffer = (char*) malloc( len * sizeof(char)); 
-  if( ! cpy_buffer )
+  tmp_buffer = (char*) malloc( len * sizeof(char)); 
+  if( ! tmp_buffer )
     return NULL;
-  bzero(cpy_buffer, len * sizeof(char));
+  bzero(tmp_buffer, len * sizeof(char));
 
-  return cpy_buffer;
-}
-
-/*
- * Set removetab[c] = 1 for all chars c in remove_chars[]
- */
-void set_removetab(char removetab[], char remove_chars[]){
-  int i;
-
-  if( ! ( removetab && remove_chars) )
-    return;
-
-  for(i = 0; remove_chars[i] ; i++)
-    removetab[ remove_chars[i] ] = 1;
+  return tmp_buffer;
 }
 
 /*
@@ -41,23 +28,25 @@ void set_removetab(char removetab[], char remove_chars[]){
  */
 void remove_from_str(char remove_chars[], char str[]){
   int  read = 0, write = 0, i = 0;
-  char removetab[128] = {'\0'}, *cpy_buffer = NULL, c = 0;
+  char removetab[128] = {'\0'}, *tmp_buffer = NULL, c = 0;
 
   if( ! (remove_chars && str) )
     return;
 
-  cpy_buffer = allocate_cpy_buffer(str);
-  if( ! cpy_buffer )
+  tmp_buffer = allocate_tmp_buffer(str);
+  if( ! tmp_buffer )
     return;
-  set_removetab(removetab, remove_chars);
+  
+  for(i = 0 ; remove_chars[i] ; i++)
+    removetab[ remove_chars[i] ] = 1;
 
   do{
     c = str[read];
     if( ! removetab[ c ] )
-      cpy_buffer[write++] = c;
+      tmp_buffer[write++] = c;
     read++;
   }while( c );
-  strcpy(str, cpy_buffer);
+  strcpy(str, tmp_buffer);
 
-  free(cpy_buffer);
+  free(tmp_buffer);
 }
