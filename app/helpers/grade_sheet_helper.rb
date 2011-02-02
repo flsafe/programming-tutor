da155fcd9e4d8c595769b2f579365bfc91176b3b
Francisco Licea <francisco.licea@gmail.com>
2011-02-02 13:14:55 -0600

    Fix bug were grade sheets with no input would crash

--- app/helpers/grade_sheet_helper.rb
+++ app/helpers/grade_sheet_helper.rb
@@ -5,10 +5,11 @@ module GradeSheetHelper
   end
 
   def to_color_points_td(test_results)
-    if test_results[:got].strip.chomp == test_results[:expected].strip.chomp
-      css_class = "green-text"
-    else
-      css_class = ""
+    css_class = ""
+    if test_results[:got] and test_results[:expected]
+      if test_results[:got].strip.chomp == test_results[:expected].strip.chomp
+        css_class = "green-text"
+      end
     end
     "<td class='#{css_class}'> #{test_results[:points]} </td>"
   end
