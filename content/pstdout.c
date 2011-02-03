fd8d97f01dc8dcbdbbda0fc3d83ba8687b37645a
Francisco Licea <francisco.licea@gmail.com>
2011-02-03 17:53:33 -0600

    Add extra info in the print message

--- content/pstdout.c
+++ content/pstdout.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 void remove_char(char c, char str[]){
-  printf("test message");
-  fprintf(stderr, "test message");
+  printf("test message from stdout");
+  fprintf(stderr, "test message from stderr");
 }
