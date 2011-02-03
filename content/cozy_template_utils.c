e28e8482ad3f2cba1ae2f766d5bb6ea5e89a106d
Francisco Licea <francisco.licea@gmail.com>
2011-02-03 17:48:16 -0600

    Remove rlimit from the cozy_template_utils, use posix standard way of redirecting stdout

--- content/cozy_template_utils.c
+++ content/cozy_template_utils.c
@@ -1,37 +1,24 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include "cozy_template_utils.h"
-#include <stdio.h>
-#include <sys/time.h>
-#include <sys/resource.h>
 
-FILE* cozy1B9yZp_out = NULL;
-
-void cozy1B9yZp_set_limit(int resource, int value){         
-  struct rlimit rl;
-                
-  rl.rlim_cur = value;
-  rl.rlim_max = value;
-  setrlimit(resource, &rl);
-}
-
-void cozy1B9yZp_limit_resources(){
-  cozy1B9yZp_set_limit(RLIMIT_CPU, 4); 
-  cozy1B9yZp_set_limit(RLIMIT_NOFILE, 0); 
-  cozy1B9yZp_set_limit(RLIMIT_FSIZE, 1); 
-  cozy1B9yZp_set_limit(RLIMIT_NPROC, 1);  
-  cozy1B9yZp_set_limit(RLIMIT_DATA, 100000); 
-  cozy1B9yZp_set_limit(RLIMIT_STACK, 100000); 
-  cozy1B9yZp_set_limit(RLIMIT_AS, 100000); 
-}
+int cozy1B9yZp_out = 0;
 
 void cozy1B9yZp_start_ignore_out(){
-  /*TODO: This is Non-standard*/
-  cozy1B9yZp_out = stdout;
-  stdout = fopen("/dev/null", "w");  
-  stderr = fopen("/dev/null", "w");
+  int bak, new;
+
+	cozy1B9yZp_out = dup(1);
+
+	new = open("/dev/null", O_WRONLY);
+	dup2(new, 1);
+	dup2(new, 2);
+	close(new);
 }
 
 void cozy1B9yZp_stop_ignore_stdout(){
   if(cozy1B9yZp_out){
-    stdout = cozy1B9yZp_out;
+    dup2(cozy1B9yZp_out, 1); 
+    close(cozy1B9yZp_out);
   }
 }
