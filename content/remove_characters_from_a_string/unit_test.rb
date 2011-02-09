064b8ca514f596d1a802e657295a7fd5d2aa3b52
Francisco Licea <francisco.licea@gmail.com>
2011-02-09 12:53:01 -0600

    Modify Remove Characters From A String to use the new idone client code

--- content/remove_characters_from_a_string/unit_test.rb
+++ content/remove_characters_from_a_string/unit_test.rb
@@ -1,49 +1,39 @@
-require 'yaml'
-require './content/cozy_unit_test'
-
-class RemoveLetterUnitTest < CozyUnitTest
-  
-  def test_remove_all_characters
-    run_with_and_expect('c ccc')
-  end
-  
-  def test_remove_first_character
-    run_with_and_expect('c caaa', 'aaa')
-  end
-  
-  def test_remove_last_character
-    run_with_and_expect('c aaac', 'aaa') 
-  end
-  
-  def test_no_characters_removed
-    run_with_and_expect('c aaa', 'aaa')
-  end
-  
-  def test_empty_string
-    run_with_and_expect('c', '') 
-  end
-
-  def test_remove_characters_from_middle
-    run_with_and_expect("abc czbzaz", "zzz")
-  end
-
-  def test_remove_all_characters
-    run_with_and_expect("xkcd xkcd", "")
-  end
-
-  def test_remove_characters_from_end
-    run_with_and_expect("xyz abcxyz", "abc")
-  end
-
-  def test_remove_characters_from_front
-    run_with_and_expect("xyz xyzabc", "abc") 
-  end
-
-  def test_remove_characters_from_middle
-    run_with_and_expect("xyz abcxyzabc", "abcabc")
-  end
+def test_remove_all_characters
+  run_with_and_expect('c ccc')
 end
-  
-test = RemoveLetterUnitTest.new("<EXEC_NAME>")
-test.start
-puts YAML.dump(test.results)
+
+def test_remove_first_character
+  run_with_and_expect('c caaa', 'aaa')
+end
+
+def test_remove_last_character
+  run_with_and_expect('c aaac', 'aaa') 
+end
+
+def test_no_characters_removed
+  run_with_and_expect('c aaa', 'aaa')
+end
+
+def test_empty_string
+  run_with_and_expect('c', '') 
+end
+
+def test_remove_characters_from_middle
+  run_with_and_expect("abc czbzaz", "zzz")
+end
+
+def test_remove_all_characters
+  run_with_and_expect("xkcd xkcd", "")
+end
+
+def test_remove_characters_from_end
+  run_with_and_expect("xyz abcxyz", "abc")
+end
+
+def test_remove_characters_from_front
+  run_with_and_expect("xyz xyzabc", "abc") 
+end
+
+def test_remove_characters_from_middle
+  run_with_and_expect("xyz abcxyzabc", "abcabc")
+end
