208d839f907afe7e3cb9bf0689268785f400bcf7
Francisco Licea <francisco.licea@gmail.com>
2011-02-09 13:13:32 -0600

    Remove Compiler@compile_to since blueberry now uses ideone for grading

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -2,8 +2,6 @@ require 'tempfile'
 
 class Compiler
 
-  @@TEMPLATE_UTILS_PATH = "content/cozy_template_utils.c"
-  
   def self.syntax_error?(code)
     Compiler.check_syntax(code)
     $? == 0 ? false : true
@@ -22,23 +20,6 @@ class Compiler
       raise "Could not write tmp scratch file to do the syntax check!"
     ensure
       f.close
-    end
-  end
-
-  def self.compile_to(code, dest_path, options="")
-    f              = Tempfile.new('exec', APP_CONFIG['work_dir'])
-    code_file_path = f.path
-    begin
-      f.write(code)
-      f.flush
-      out = `gcc #{options} -fstack-protector-all -x c -g -o #{dest_path} #{code_file_path} #{@@TEMPLATE_UTILS_PATH} 2>&1`
-      if $? != 0
-        Rails.logger.error("Could not compile:\n #{out}")
-        raise "Could not compile solution! #{out}"
-      end
-      true
-    ensure
-      f.close      
     end
   end
 end
