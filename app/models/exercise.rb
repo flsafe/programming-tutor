c661a3962a62b85c85bacc5a904c6243bff3db96
Francisco Licea <francisco.licea@gmail.com>
2011-02-10 12:28:22 -0600

    Remove exercise methods for supporting nested forms

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -11,99 +11,17 @@ class Exercise < ActiveRecord::Base
 
   belongs_to :exercise_set  
   
-  after_update :save_associates
-  
   validates_presence_of :title, :description, :problem, :tutorial, :minutes, :unit_tests, :exercise_set
   validates_associated  :hints, :unit_tests, :exercise_set, :solution_templates
   
   validates_uniqueness_of :title
-    
-  def self.recommend(user_id, how_many)
-    ex_no_grades = Exercise.find(:all).find_all {|e| e.grade_sheets.empty?}
-    exercises    = Exercise.find(:all, :conditions=>['grade_sheets.user_id <> ?', user_id], :joins=>:grade_sheets)
-    exercises   += ex_no_grades
 
-    how_many     = clamp(how_many, 0, exercises.size)
-    indices      = random_indices(how_many, exercises.size)
-    indices.collect {|i| exercises[i]}
-  end
- 
   def sample?
     titles = APP_CONFIG['demo_exercise_titles']
     titles.detect {|t| t == self.title} != nil
   end
 
-  def new_solution_template_attributes=(attributes)
-    new_attributes_for(:solution_templates, attributes)
-  end
-  
-  def new_hint_attributes=(attributes)
-    new_attributes_for(:hints, attributes)
-  end
-  
-  def new_unit_test_attributes=(unit_test_attributes)
-    new_attributes_for(:unit_tests, unit_test_attributes)
-  end
-
-  def new_exercise_set_attributes=(new_exercise_set_attributes)
-    #A new exercise set hash is not always returned from the client, sometimes one is chosen from the select dropdown.
-    self.exercise_set = build_exercise_set new_exercise_set_attributes unless new_exercise_set_attributes['title'].blank?
-  end
-  
-  def existing_solution_template_attributes=(template_attributes)
-    existing_attributes_for(:solution_templates, template_attributes)
-  end
-  
-  def existing_hint_attributes=(hint_attributes)
-   existing_attributes_for(:hints, hint_attributes)
-  end
-  
-  def existing_unit_test_attributes=(unit_test_attributes)
-    existing_attributes_for(:unit_tests, unit_test_attributes)
-  end
-  
   def get_stat(name)
     Statistic.get_stat("exercise.#{name}", self.id) || 0
-  end
-  
-  protected
-    
-  def new_attributes_for(association, attributes)
-    associates = self.send(association)
-    attributes.each do |atts|
-      associates.build atts
-    end
-  end
-  
-  def existing_attributes_for(association, associated_attributes)
-    associates = self.send(association)
-    associates.reject(&:new_record?).each do |associate|
-      attributes = associated_attributes[associate.id.to_s]
-      if attributes
-        associate.attributes = attributes
-      else
-        associates.delete(associate)
-      end
-    end
-  end
-  
-  def save_associates
-    hints.each {|h| h.save(false)}
-    unit_tests.each {|u| u.save(false)}
-    solution_templates.each {|s| s.save(false)}
-    exercise_set.save(false)
-  end
-  
-  def self.random_indices(how_many, size_of_array)
-    indices = []
-    while indices.size != how_many
-      index = rand(size_of_array)
-      indices << index if not indices.include?(index)
-    end
-    indices
-  end
-  
-  def self.clamp(v, min, max)
-    [[v, max].min, min].max
   end
 end
