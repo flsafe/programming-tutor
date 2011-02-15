34df0c3d157d299f31155315cd426ad7acb159bf
Francisco Licea <francisco.licea@gmail.com>
2011-02-15 14:54:43 -0600

    4 passing steps, first logical error

--- features/step_definitions/beta_invite_steps.rb
+++ features/step_definitions/beta_invite_steps.rb
@@ -1,4 +1,4 @@
 Then /^there should be a new invite for "([^"]*)"$/ do |email|
-  BetaInvite.count(['email = ?', email]).should == 1
+  BetaInvite.count(:conditons=>['email = ?', email]).should == 1
 end
 
