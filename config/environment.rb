4e0e53f948f7796225e1c98de45418cd1ec3ec4d
Francisco Licea <francisco.licea@gmail.com>
2011-02-19 10:47:32 -0600

    Change the domain in the smtp settings to mcstacker.blueberrytree.ws

--- config/environment.rb
+++ config/environment.rb
@@ -52,7 +52,7 @@ Rails::Initializer.run do |config|
   config.action_mailer.smtp_settings = {
     :address => "localhost",
     :port => 25,
-    :domain=> "blueberrytree.ws"
+    :domain=> "mcstacker.blueberrytree.ws"
   }
 end
 
