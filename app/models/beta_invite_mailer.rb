337d90d259dc77b5179e309fb98e39b95a987d0c
Francisco Licea <francisco.licea@gmail.com>
2011-02-20 13:14:05 -0600

    Fix 'from' in beta invite email

--- app/models/beta_invite_mailer.rb
+++ app/models/beta_invite_mailer.rb
@@ -2,7 +2,7 @@ class BetaInviteMailer < ActionMailer::Base
   def invite(inv)
     subject    'BlueberryTree Beta Invite'
     recipients inv.email 
-    from       '<Francisco> beta@blueberrytree.ws'
+    from       'Francisco Licea <beta@blueberrytree.ws>'
     sent_on    Time.now 
     
     body       :token=> inv.token 
