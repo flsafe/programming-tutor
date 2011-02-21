754fdaa16918b9805cb38989a92c10de1f7fc4fc
Francisco Licea <francisco.licea@gmail.com>
2011-02-21 17:07:58 -0600

    Get the stack linked list scenario to pass

--- content/implement_a_stack_using_a_linked_list/unit_test.rb
+++ content/implement_a_stack_using_a_linked_list/unit_test.rb
@@ -1,18 +1,18 @@
 
 def test_push_one_element 
-  run_with_and_expect('+1', exp("0 1"))
+  run_with_and_expect('+1', exp("1 1"))
 end
 
 def test_pop_empty_stack
-  run_with_and_expect('-', exp("1"))
+  run_with_and_expect('-', exp("0"))
 end
 
 def test_pop_and_push_one_element
-  run_with_and_expect('+1-', exp('00'))
+  run_with_and_expect('+1-', exp('11'))
 end
 
 def test_push_multiple 
-  run_with_and_expect('+1+2+3+4', exp('0000 4321'))
+  run_with_and_expect('+1+2+3+4', exp('1111 4321'))
 end
 
 def exp(str)
