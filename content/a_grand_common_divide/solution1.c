dbb9029ea894b73cc3ed0a355b5042b8106d2e6d
Francisco Licea <francisco.licea@gmail.com>
2011-03-07 09:54:12 -0600

    Fix bug in solution1.c

--- content/a_grand_common_divide/solution1.c
+++ content/a_grand_common_divide/solution1.c
@@ -3,8 +3,10 @@ unsigned int gcd(unsigned int a, unsigned int b){
 
   if (a == 0 && b == 0)
     return 0;
-  else if (a == 0 || b == 0)
-    return 1;
+  else if (a == 0)
+    return b;
+  else if (b == 0)
+    return a;
 
   i = a < b ? a : b;
   
