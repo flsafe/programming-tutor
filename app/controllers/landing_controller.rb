f5ab14c5ca3ed374edbbf21d373d3d1cac1d5130
Francisco Licea <francisco.licea@gmail.com>
2011-05-04 17:55:28 -0500

    Users that have a valid session now skip the landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,11 +1,8 @@
 class LandingController < ApplicationController
   
   def index
-    
+    if current_user_session_and_not_anonymous
+      redirect_to :controller=>:overview
+    end
   end
-  
-  def motivation
-    
-  end
-  
 end
